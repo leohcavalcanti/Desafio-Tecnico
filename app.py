ae12c124ea48fb212f5ea32103767effa197f402
leohcavalcanti <leonardocaturyty@hotmail.com>
2026-05-19 20:01:45 -0300

    feat: Adição da página web na rota (/)

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from app.models import db
 from app.controllers.plano_aula_controllers import plano_aula_bp
 from dotenv import load_dotenv
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 
 db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
@@ -31,6 +31,9 @@ app.register_blueprint(plano_aula_bp, url_prefix="/api")
 def health_check():
     return {"status": "ok", "message": "Servidor rodando normalmente!"}, 200
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
